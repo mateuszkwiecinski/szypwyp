56bf0a8a5419fa0d27a5d305d27ead9cef9e7c72
Mateusz Kwieciński <spamelami@gmail.com>
2018-08-18 23:24:21 +0200

    fcuk crushlytics

--- presentation/src/main/kotlin/pl/ccki/szypwyp/presentation/PresentationAplication.kt
+++ presentation/src/main/kotlin/pl/ccki/szypwyp/presentation/PresentationAplication.kt
@@ -1,7 +1,6 @@
 package pl.ccki.szypwyp.presentation
 
 import dagger.android.support.DaggerApplication
-import io.fabric.sdk.android.Fabric
 
 abstract class PresentationAplication : DaggerApplication() {
     override fun onCreate() {
