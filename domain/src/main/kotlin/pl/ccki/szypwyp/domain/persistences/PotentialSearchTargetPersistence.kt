e4b21d9bfec5bc3f45378da7f0af78c7510f70f6
Mateusz Kwieciński <spamelami@gmail.com>
2018-11-04 22:05:03 +0100

    Add app Icon

--- domain/src/main/kotlin/pl/ccki/szypwyp/domain/persistences/PotentialSearchTargetPersistence.kt
+++ domain/src/main/kotlin/pl/ccki/szypwyp/domain/persistences/PotentialSearchTargetPersistence.kt
@@ -2,6 +2,7 @@ package pl.ccki.szypwyp.domain.persistences
 
 import io.reactivex.Maybe
 import pl.ccki.szypwyp.domain.models.LatLng
+import pl.ccki.szypwyp.domain.repositories.SearchConfigRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,4 +11,6 @@ interface PotentialSearchTargetPersistence : DefaultPersistence<LatLng> {
 }
 
 @Singleton
-class InMemorySearchTargetPersistence @Inject constructor() : BehaviorSubjectBasedPersistence<LatLng>(), PotentialSearchTargetPersistence
+class InMemorySearchTargetPersistence @Inject constructor(
+    private val repository: SearchConfigRepository
+) : BehaviorSubjectBasedPersistence<LatLng>({ repository.target }), PotentialSearchTargetPersistence
